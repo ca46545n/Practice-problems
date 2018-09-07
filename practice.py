11648ccdf719e96e1469f48b9ef97c0056411064
Chabelis Alegria <calegria@Chabelis-MBP.local>
2018-09-07 15:56:21 -0400

    I completed ma dice game

--- practice.py
+++ practice.py
@@ -1,1 +1,20 @@
-print 'hello world'
+import random
+
+print 'Do you wanna roll dice homie G ?'
+yes = True
+answer = raw_input('yes or no: ')
+if (answer == 'yes'):
+    while(yes):
+        print random.randint(1,6)
+        print 'Do you wanna roll again  ?'
+        answer = raw_input('yes or no: ')
+        # print(answer)
+        if (answer == 'yes'):
+            yes = True
+        else:
+            yes = False
+else:
+    print 'leave me alone'
+
+if(yes == False):
+    print 'leave me alone'
