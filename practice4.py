e52679a2329e8ab9e743cd435ff3586ae7511ad1
Chabelis Alegria <calegria@Chabelis-MBP.local>
2018-09-12 14:28:57 -0400

    Date and Time

--- practice4.py
+++ practice4.py
@@ -1,15 +1,22 @@
 from datetime import datetime
+import time
+   #random.randint(1,60)
+
 
 odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19,
         21, 23, 25, 27, 29, 31, 33, 35, 37, 39,
         41, 43, 45, 47, 49, 51, 53, 55, 57, 59]
 
-right_this_minute = datetime.today().minute
 
-if right_this_minute in odds:
-    print("this minute seems a little odd.  ")
+while(True):
+    right_this_minute = datetime.today()
+    print(right_this_minute.strftime("%I:%M"))
+    if right_this_minute.minute in odds:
+        print("this minute seems a little odd.  ")
+    else:
+        print(" Not an odd minute. ")
+    time.sleep(60)
 
-else:
-    print(" Not an odd minute. ")
+
 
 #challenge complete
